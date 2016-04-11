da9bfba9f8111fa62ff4b0387e3b2faf8f367855
Jacob Bieker <jacob@bieker.tech>
2016-04-11 12:04:00 -0700

    Create stub functions and skeleton of program

--- perpendicular-least-squares.py
+++ perpendicular-least-squares.py
@@ -3,3 +3,36 @@ import os, sys
 import numpy
 from multiprocessing import Pool
 
+
+def line_solve():
+    # TODO: Find the Least Squares for a line
+    return 0
+
+
+def plane_solve():
+    # TODO: Find the least Squares for a plane
+    return 0
+
+
+def read_clusters():
+    # TODO: Read in the files containing the cluster points
+    return 0
+
+
+def determine_type():
+    # TODO: Determine the type (i.e. Plane or Line) to solve for
+    return 0
+
+
+def initial_guess(largest_cluster, type):
+    if type == "line":
+        return 0
+    elif type == "plane":
+        return 0
+    # TODO: Get an inital guess from the perpendicular least squares method from the largest cluster
+    return
+
+
+if __name__ == "__main__":
+    arguments = str(sys.argv)
+    print(arguments)
