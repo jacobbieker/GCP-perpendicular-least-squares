1d80724f9bcabb43e191b082cd145eb60d8a8c2f
Jacob Bieker <jacob@bieker.tech>
2016-04-13 14:26:46 -0700

    Add program description from iterfitlin.cl

--- perpendicular-least-squares.py
+++ perpendicular-least-squares.py
@@ -4,6 +4,46 @@ import numpy
 from multiprocessing import Pool
 from astropy.io import fits
 
+# Fit plane or line iteratively
+#   if more than one cluster, each cluster in separate STSDAS table
+#
+# Residuals are calculated according to resalgo
+#      per:  perpendicular to the fitted relation
+#            delta = (y - a*x1 - b*x2 - c)/sqrt(1+a^2+b^2)
+#      y  :  in y     delta = (y - a*x1 - b*x2 - c)
+#      x1 :  in x1
+#      x2 :  in x2
+#
+# Minization algorithms:  (minalgo)
+#    quartile : distance between upper (0.75) quartile point
+#               and lower (0.25) quartile point
+#    delta100 : SUM abs(res)
+#    delta60  : SUM abs(res) of the central 60 percent of distribution
+#    rms100   : SUM res^2
+#    rms60    : SUM res^2 of the central 60 percent of distribution
+#
+# Zero points : median (use with delta and quartile)
+#               mean   (use with rms)
+#
+# Bootstrap uncertainties on coefficients if nboot>0
+# Seed for random is in each step   seed-(nboot-step)
+#
+# WARNING: This is a rather slow algorithm.
+# FP for 200 galaxies in 10 clusters takes 5min on Sparc 10
+# if restart=yes and nboot=0 (no uncertainties)
+#
+# Version: 21.06.95
+#          16.10.95  Solaris roeskva
+#          14.02.96  changed resalgo=y initial values, faster conv.
+#          10.11.97  use rename instead of trename in 2.11
+#          21.01.98  bootstrap changed to the proper way of doing it
+#          06.03.98  fixed bug in bootstrap for 2 parameter fits
+#                    improved random number generation
+#          27.07.98  stop if itera>3*maxiter || niterb>3*maxiter
+#                    no matter the other parameter's niter
+#          30.03.04  handle INDEF automatically, works under IRAF 2.12.2 Redhat9.0
+# Inger Jorgensen, Gemini Observatory
+# e-mail: inger@gemini.edu
 
 def random_number(number, seed):
     rand_nums = []
