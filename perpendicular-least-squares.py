d87f69813dd602496b6f4128cb09e7aad9608858
Jacob Bieker <jacob@bieker.tech>
2016-09-15 10:01:58 +0200

    Implement Determining and Changing Coefficients
    Closes #7, Closes #8, Closes #9
    
     Need to either switch to global variables, like in original code, or figure out a better way of moving the variables around. Still not decided, so while it closes the different issues, it creates its own issue #15

--- perpendicular-least-squares.py
+++ perpendicular-least-squares.py
@@ -113,52 +113,6 @@ def min_rms(percentage):
 
 
 def zeropoint(cluster, type_solution, res_choice, y_col, x1_col, x2_col, a_factor, b_factor):
-    """
-
-    # derive the zero points and calculate the residuals
-    # the zero points are median or mean as defined by n_zeropoint
-    n_norm=1.                         # min in y
-    if(n_resalgo=="per")
-      n_norm=sqrt(1.+n_a**2+n_b**2)   # min perpendicular
-    if(n_resalgo=="x1")
-      n_norm=-1.*n_a                  # min in x1
-    if(n_resalgo=="x2")
-      n_norm=-1.*n_b                  # min in x2
-
-    tcalc(tmpall,"res","0.",colfmt="f6.3")
-
-    for(n_i=1 ; n_i<=n_clus ; n_i+=1) {
-    # delta y
-     n_expression = "("//n_recol//"-"//n_a//"*"//n_sigcol//"-"//n_b//"*"//n_Iecol//")"
-     print(n_expression, > tmpexp)
-     tcalc(tmpall,"z"//n_i,"@"//tmpexp,colfmt="f6.3")
-     delete(tmpexp,verify=no)
-     n_expression="z"//n_i//"*(nclus=="//n_i//")+1000.*(nclus!="//n_i//")"
-     print(n_expression, >tmpexp)
-     tcalc(tmpall,"z"//n_i,"@"//tmpexp,colfmt="f6.3")
-     delete(tmpexp,verify=no)
-     tstat(tmpall,"z"//n_i,lowlim=INDEF,highlim=100., >> "/dev/null")
-     if(n_zeropoint=="median")
-       n_zero=tstat.median
-     else
-       n_zero=tstat.mean
-    # printf("Zero point for cluster  %-3d : %8.5f\n",n_i,n_zero)
-    # residuals normalized
-     n_expression="((z"//n_i//"-"//n_zero//")*(nclus=="//n_i//"))/"//n_norm//"+1000.*(nclus!="//n_i//")"
-     print(n_expression, > tmpexp)
-     tcalc(tmpall,"r"//n_i,"@"//tmpexp,colfmt="f6.3")
-     delete(tmpexp,verify=no)
-     n_expression="res+((z"//n_i//"-"//n_zero//")*(nclus=="//n_i//"))/"//n_norm
-     print(n_expression, > tmpexp)
-     tcalc(tmpall,"res","@"//tmpexp,colfmt="f6.3"
-     delete(tmpexp,verify=no)
-    }
-
-    RMA coefficients: a_factor, b_factor
-    :param cluster:
-    :param type_solution:
-    :return:
-    """
 
     # Adds a column full of zeros to the FITS table for use in residual
     residual_column = fits.Column(name='res', format='f6.3', array=0)
@@ -249,12 +203,12 @@ def plane_solve():
     return 0
 
 
-def read_clusters(filename, table_list, galaxy_name, group_name, y_col, x1_col, x2_col):
+def read_clusters(filename, solve_plane, galaxy_name, group_name, y_col, x1_col, x2_col):
     '''
     Reads in the FITS file and creates one table from the different tables and counts the
     total number of galaxies, for use later
     :param filename: Name of the FITS file
-    :param table_list:
+    :param solve_plane: Boolean whether x2_col should be counted or not
     :param galaxy_name: Column name for the galaxy
     :param group_name: Column name for the group
     :param y_col: Column name for y
@@ -271,7 +225,7 @@ def read_clusters(filename, table_list, galaxy_name, group_name, y_col, x1_col, 
             table = Table.read(filename, format="fits", hdu=hdu_num)
             cluster_number += 1
             hdu_num += 1
-            if x2_col != "":
+            if solve_plane:
                 new_table = Table(table, names=(galaxy_name, group_name, y_col, x1_col, x2_col))
             else:
                 new_table = Table(table, names=(galaxy_name, group_name, y_col, x1_col))
@@ -311,6 +265,96 @@ def determine_uncertainty(solutions):
     return 0
 
 
+def change_coefficients():
+    m_a = a_factor_in / 200.0
+    m_b = b_factor_in / 200.0
+    if (a_factor <= m_a or a_iterations > max_iterations) and ((b_factor <= m_b or b_iterations > max_iterations) or solve_plane ) or ( a_iterations > 3 * max_iterations or b_iterations > 3 * max_iterations ):
+        if not restart_factor:
+            flow_end = True
+            flow_print = True
+            a_in = a_out
+            b_in = b_out
+            next_res()
+        else:
+            restart_factor = False
+            a_in = a_out
+            b_in = b_out
+            a_factor_in /= 100.0
+            b_factor_in /= 100.0
+            max_iterations /= 2.0
+            if flow_print:
+                '''
+                if(n_flprint) {
+                      print("")
+                      printf("Restarting with (a,b)=(%7.4f,%7.4f)\n",n_a,n_b)
+                      printf("    (n_facta,n_bfact)=(%7.4f,%7.4f)\n",n_factain,n_factbin)
+                      print("")
+                    }
+                '''
+            restart()
+
+    # Change Coefficients
+    if flow_a:
+        a_in *= 1.0 + sig_a * a_factor
+        a_iterations += 1
+    if flow_b:
+        b_in *= 1.0 * sig_b * b_factor
+        b_iterations += 1
+    next_res()
+
+    return 0
+
+
+def restart():
+    return 0
+
+def next_res():
+    return 0
+
+def determine_change_coefficients(a_iterations, b_iterations, a_in, b_in, delta_in, very_low_in, very_high_in, minimization_algorithm):
+    if a_iterations == 1 and b_iterations == 1:
+        a_out = a_in
+        b_out = b_in
+        delta_out = delta_in
+        if minimization_algorithm == "quartile":
+            very_low_out = very_low_in
+            very_high_out = very_high_in
+    else:
+        if delta_in <= delta_out and a_factor > a_factor_in / 200.0 and (b_factor > b_factor_in / 200.0 or solve_plane):
+            a_out = a_in
+            b_out = b_in
+            delta_out = delta_in
+            flow_first = False
+            if minimization_algorithm == "quartile":
+                very_low_out = very_low_in
+                very_high_out = very_high_in
+            change_coefficients()
+        if delta_in > delta_out and a_factor > a_factor_in / 200.0 and (b_factor > b_factor_in / 200.0 or solve_plane):
+            # Change the current coefficients back to previous values
+            a_in = a_out
+            b_in = b_out
+            if flow_a and (a_iterations == 1 or flow_first):
+                sig_a = -sig_b
+                flow_first = False
+                change_coefficients()
+            if flow_b and (b_iterations == 1 or flow_first):
+                sig_b = -sig_b
+                flow_first = False
+                change_coefficients()
+            if (a_iterations > 1 or b_iterations > 1) and not flow_first:
+                if not solve_plane:
+                    # Change the other coefficient
+                    flow_a = not flow_a
+                    flow_b = not flow_b
+                flow_first = True
+                if flow_a and a_iterations > 1:
+                    sig_a = -sig_a
+                    a_factor /= 2.0
+                if flow_b and b_iterations > 1:
+                    b_factor /= 2.0
+                change_coefficients()
+
+
 if __name__ == "__main__":
 
     filename = str(input("Enter the filename containing the cluster(s): ")).strip()
@@ -319,7 +363,7 @@ if __name__ == "__main__":
     res_choice = str(input("Residual to minimize (per,y,x1,x2): ")).strip() or "per"
     y_col = str(input("Column name for y: ")).strip() or "lre_GR_sc"
     x1_col = str(input("Column name for x1: ")).strip() or "lsig_re"
-    x2_col = str(input("Column name for x2 (optional): ")).strip() or ""
+    x2_col = str(input("Column name for x2 (optional): ")).strip()
     zeropoint_choice = input("Zeropoints (median, mean): ") or "median"
     galaxy_name = str(input("Column name for galaxy: ")).strip()
     group_name = str(input("Column name for group: ")).strip()
@@ -345,6 +389,7 @@ if __name__ == "__main__":
     if not x2_col:
         # Only use two parameters
         factor_change_b = 0.0
+        solve_plane = False
         line_solve()
     else:
         plane_solve()
