42e4fb1a76cf91becbbb920e7e4b573678a540e3
Jacob Bieker <jacob@bieker.tech>
2016-12-06 09:34:23 +0100

    Start on fitplan conversion
    Does not seem like the ODR process will give the same output as before, still looking into it, but starting to look like I have to change the whole thing over to Python.
    
    Another thing pointing to that, Scipy's ODR seems to use a different algorithm, the trust-region Levenberg-Marquardt-type algorithm, compared to Jorgenson's LDLT method.
    
    Still going to work on converting to the low level odr function on the scipy side to see if I can get the output needed from fitplan. Also needs to see what exactly fitplan outputs vs what scipy can.
    
    Essentially, more testing needs to be done, but while waiting for that, some conversion is continuing

--- fitplan.py
+++ fitplan.py
@@ -11,23 +11,31 @@ dim=20
 dim_two=21
 dim_three=420
 scale=22.0
-x = [][] # Values of dim_one by nmax_one
-mat = [][] # Values of dim by dim_two
-vec = [] # Size of dim
+x = np.ndarray(shape=(dim_one, nmax_one), dtype=float, order='F')
+mat = np.ndarray(shape=(dim, dim_two), dtype=float, order='F') # Values of dim by dim_two
+vec = np.ndarray(shape=dim, dtype=float, order='F') # Size of dim
 #  coef(DIM1,DIM1)
-coef = [][]
+coef = np.ndarray(shape=(dim_one, dim_one), dtype=float, order='F')
 # covmat(DIM0,DIM,DIM2), scoef(DIM1,DIM1)
-covmat = [][][]
-scoef = [][]
+covmat = np.ndarray(shape=(dim_zero, dim, dim_two), dtype=float, order='F')
+scoef = np.ndarray(shape=(dim_one, dim_one), dtype=float, order='F')
 # var(DIM1)
-var = []
+var = np.ndarray(shape=dim_one, dtype=float, order='F')
 # work(DIM3), res(NMAX1)
-work = []
-res = []
+work = np.ndarray(shape=(dim_three), dtype=float, order='F')
+res = np.ndarray(shape=(nmax_one), dtype=float, order='F')
 # real cond
 cond = 0.0
 
 # integer i, errcode, norder, info
+i = 0
+errcode = 0
+norder = 0
+info = 0
 # integer nclus, nlda, ntotal, nfit
-
+nclus = 0
+nlda = 0
+ntotal = 0
+nfit = 0
 # logical vflag
+vlfag = False
